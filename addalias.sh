7053f44e5b5d172c67eddc4fc91b8a017af1827a
Glarity <tolson519@gmail.com>
2018-05-20 02:49:50 -0700

    Basic adding alias to ~/.bash_aliases

---
+++ addalias.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+function addCommand() {
+	COMMAND="${*:2}"
+	ENTRY="alias $1='$COMMAND'"
+	if grep -Fxq "$ENTRY" ~/.bash_aliases
+	then
+		#TODO Check if name exists with different command
+		echo "Command already exists."
+	else
+		echo $ENTRY >> ~/.bash_aliases
+		echo "Command alias added! Restart terminal instance for effect."
+	fi
+}
+
+if [[ $# -ge 1 ]]; then
+	addCommand $*
+else
+	echo "Usage: addalias [ALIASNAME] [COMMAND]"
+	echo "Example: addalias test git"
+fi
