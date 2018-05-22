7fa10ebe255deb6866b9d609092e58d47530b107
Eric <ericolsonnv@gmail.com>
2018-05-21 19:54:51 -0700

    Updated syntax to match google style guide

--- addalias.sh
+++ addalias.sh
@@ -1,21 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 function addCommand() {
-	COMMAND="${*:2}"
-	ENTRY="alias $1='$COMMAND'"
-	if grep -Fxq "$ENTRY" ~/.bash_aliases
-	then
+	local command="${*:2}"
+	local entry="alias $1='${command}'"
+
+	if grep -Fxq "${entry}" ~/.bash_aliases; then
 		#TODO Check if name exists with different command
 		echo "Command already exists."
 	else
-		echo $ENTRY >> ~/.bash_aliases
-		source ~/.bash_aliases
+		echo ${entry} >> ~/.bash_aliases && source ~/.bash_aliases
 		echo "Command alias added!"
 	fi
 }
 
 if [[ $# -ge 1 ]]; then
-	addCommand $*
+	addCommand $@
 else
 	echo "Usage: addalias [ALIASNAME] [COMMAND]"
 	echo "Example: addalias test git"
